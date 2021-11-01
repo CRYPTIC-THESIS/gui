93c5f060aa8ce0d3970feab7c661d3624ed9a3b0
Jeremy Dalay <jeremydalay@gmail.com>
2021-11-02 00:28:57 +0800

    Update

--- admin/model/training_model.py
+++ admin/model/training_model.py
@@ -6,33 +6,33 @@ dataset_all = pd.read_csv('admin/model/dataset.csv')
 crypto = np.array(dataset_all['Cryptocurrency'])
 crypto = np.unique(crypto)
 
-#for i in range(len(crypto)):
-data = dataset_all.loc[dataset_all['Cryptocurrency'] == crypto[0]]
-dataset = pd.DataFrame()
-dataset['Close'] = data['Closing']
-dataset['Open'] = data['Open']
-dataset['High'] = data['High']
-dataset['Low'] = data['Low']
-dataset['Twitter'] = data['Twitter Volume']
-dataset['Reddit'] = data['Reddit Volume']
-dataset['Google'] = data['GoogleTrends']
+for i in range(len(crypto)):
+    data = dataset_all.loc[dataset_all['Cryptocurrency'] == crypto[i]]
+    dataset = pd.DataFrame()
+    dataset['Close'] = data['Closing']
+    dataset['Open'] = data['Open']
+    dataset['High'] = data['High']
+    dataset['Low'] = data['Low']
+    dataset['Twitter'] = data['Twitter Volume']
+    dataset['Reddit'] = data['Reddit Volume']
+    dataset['Google'] = data['GoogleTrends']
 
-dataset = dataset.fillna(0)
-Y = np.array(data['Date'])
-X = np.array(dataset['Close'])
+    dataset = dataset.fillna(0)
+    Y = np.array(data['Date'])
+    X = np.array(dataset['Close'])
 
-dataset = np.array(dataset)
+    dataset = np.array(dataset)
 
-#print(dataset)
+    #print(dataset)
 
-if(crypto[0]=='BTC'):
-    btc_model = nn.cryptic(crypto[0])
-    btc_loss,btc_trained = btc_model.train(300,dataset,X)
-elif(crypto[0]=='ETH'):
-    eth_model = nn.cryptic(crypto[0])
-    eth_loss,eth_trained = eth_model.train(300,dataset,X)
-elif(crypto[0]=='DOGE'):
-    doge_model = nn.cryptic(crypto[0])
-    doge_loss,doge_trained = doge_model.train(300,dataset,X)
-else:
-    print('Invalid Crypto')
+    if(crypto[i]=='BTC'):
+        btc_model = nn.cryptic(crypto[i])
+        btc_loss,btc_trained = btc_model.train(300,dataset,X)
+    elif(crypto[i]=='ETH'):
+        eth_model = nn.cryptic(crypto[i])
+        eth_loss,eth_trained = eth_model.train(300,dataset,X)
+    elif(crypto[i]=='DOGE'):
+        doge_model = nn.cryptic(crypto[i])
+        doge_loss,doge_trained = doge_model.train(300,dataset,X)
+    else:
+        print('Invalid Crypto')
