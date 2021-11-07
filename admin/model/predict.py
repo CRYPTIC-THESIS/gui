dfef8e1b5cf9ebdd9fb10a7639d548cf7ac86e20
Jeremy Dalay <jeremydalay@gmail.com>
2021-11-07 20:11:32 +0800

    UPDATE

--- admin/model/predict.py
+++ admin/model/predict.py
@@ -2,11 +2,16 @@ import CRYPTIC_module as nn
 import numpy as np
 cryptos = np.zeros(3)
 pred = np.zeros(14)
+pred_list = np.zeros((len(cryptos),14))
 data = np.zeros(3)
 crypto = cryptos
 net = nn.cryptic()
-for i in range(14):
-    last =  net.predict_crypto(data,crypto)
-    data = data[:2]
-    data[3] = last
-    pred[i] = last
+x = 0
+for crypto in cryptos:
+    for i in range(14):
+        last =  net.predict_crypto(data,crypto)
+        data = data[:2]
+        data[3] = last
+        pred[i] = last
+    pred_list[x] = pred
+    x+=1
